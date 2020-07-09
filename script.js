6439a2af14defdad75d34643a908d277f5efca98
Håkon Johansen <haycon@protonmail.com>
2020-07-09 22:55:50 +0200

    Version 3.0

--- script.js
+++ script.js
@@ -7,23 +7,50 @@ function addDiv () {
     currentDiv.appendChild(newDiv);
     newDiv.appendChild(comment);
     newDiv.setAttribute("class","square")
-
+    
+    newDiv.style.width = (100-2)+"px";
+    newDiv.style.height= (100-2)+"px";
     newDiv.addEventListener("mouseenter", () => {
         newDiv.style.background = "black";
     })
 
 }
 
-function comment(){
-    var c = document.createComment("HELLO");
-    document.body.appendChild(c);
-}
-
 function reset () {
-    let s = document.getElementsByClassName("square") //querySelectorAll(".square");
-    for (let i=0; i<16; i++){
+    let s = document.getElementsByClassName("square") 
+
+    for (let i=0; i<s.length; i++){
         s[i].style.background="white"
     }
+    
+}
+
+function test() {
+    let amount = prompt("How many squares would you like the square to be?")
+    let side = 400/amount
+    let s = document.getElementsByClassName("square");
+
+    while(s.length>0){
+        s[0].parentNode.removeChild(s[0]);
+    }
+
+    for(let i =0; i<amount*amount; i++){
+        let newDiv = document.createElement("div");
+        let currentDiv = document.getElementById("main");
+        let comment = document.createComment("");
+        
+        newDiv.style.width = (side-2)+"px";
+        newDiv.style.height= side-2+"px";
+        currentDiv.appendChild(newDiv);
+        newDiv.appendChild(comment);
+        newDiv.setAttribute("class","square")
+
+        newDiv.addEventListener("mouseenter", () => {
+            newDiv.style.background = "black";
+        })
+    }
+    
+
 }
 
 for(let i = 0; i<16; i++){
